198d844ab428909545794c684b349252c9151d6c
Martin89PL <marcingladkowski@gmail.com>
2021-01-16 18:52:27 +0100

    some first exercise

--- src/Objects/index.js
+++ src/Objects/index.js
@@ -1,1 +1,12 @@
 console.log('objects')
+
+/** 
+ * Create an object literal exercise:
+ * - Create object with using literal expression
+ * with property firstname (string), lastname (string), age (integer)
+ * - Add method `greet` to return an welcome message contains firstname
+ * and last name
+ * - list in console all properties using Object.keys()
+ * - modify firstname and lastname of existing object
+ * - Try to copy object to another variable! - be carefule of reference! 
+ */
